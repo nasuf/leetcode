ed88cf3f8a1bb537894eb55dbf7c32d2f6120df5
pocketContributor <842100455@qq.com>
2021-09-03 20:00:46 +0800

    leetcode [136] 只出现一次的数字 - 位运算

---
+++ src/leetcode/editor/cn/SingleNumber.java
@@ -0,0 +1,59 @@
+//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+//
+// 说明： 
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+//
+// 示例 1: 
+//
+// 输入: [2,2,1]
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入: [4,1,2,1,2]
+//输出: 4 
+// Related Topics 位运算 数组
+
+
+package leetcode.editor.cn;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        Solution solution = new SingleNumber().new Solution();
+        System.out.println(solution.singleNumber(new int[] {1,2,1,2,3}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        /**
+         * 任何数和 00 做异或运算，结果仍然是原来的数，即 a ⊕ 0 = a。
+         * 任何数和其自身做异或运算，结果是 0，即 a ⊕ a = 0。
+         * 异或运算满足交换律和结合律，即 a ⊕ b ⊕ a = b ⊕ a ⊕ a = b ⊕ ( a ⊕ a ) = b ⊕ 0 = b。
+         */
+        public int singleNumber(int[] nums) {
+            int single = 0;
+            for (int num : nums) {
+                single ^= num;
+            }
+            return single;
+        }
+        /*public int singleNumber(int[] nums) {
+            HashSet<Integer> set = new HashSet<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (set.contains(nums[i])) {
+                    set.remove(nums[i]);
+                } else {
+                    set.add(nums[i]);
+                }
+            }
+            return (Integer) set.toArray()[0];
+        }*/
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
