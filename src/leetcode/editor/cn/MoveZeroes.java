a80df514a3c56048238a7bd5397f5c86e889af79
pocketContributor <842100455@qq.com>
2021-09-05 20:29:11 +0800

    leetcode [283] 移动零

---
+++ src/leetcode/editor/cn/MoveZeroes.java
@@ -0,0 +1,63 @@
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+//
+// 示例: 
+//
+// 输入: [0,1,0,3,12]
+//输出: [1,3,12,0,0] 
+//
+// 说明: 
+//
+// 
+// 必须在原数组上操作，不能拷贝额外的数组。 
+// 尽量减少操作次数。 
+// 
+// Related Topics 数组 双指针
+
+
+package leetcode.editor.cn;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class MoveZeroes {
+    public static void main(String[] args) {
+        Solution solution = new MoveZeroes().new Solution();
+        solution.moveZeroes(new int[]{0, 1, 0, 3, 12});
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void moveZeroes(int[] nums) {
+            int n = nums.length, left = 0, right = 0;
+            while (right < n) {
+                if (nums[right] != 0) {
+                    swap(nums, left, right);
+                    left++;
+                }
+                right++;
+            }
+        }
+
+        public void swap(int[] nums, int left, int right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+        }
+        /*public void moveZeroes(int[] nums) {
+            LinkedList<Integer> list = new LinkedList<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    list.add(nums[i]);
+                }
+            }
+            for (int i = 0; i < list.size(); i++) {
+                nums[i] = list.get(i);
+            }
+            for (int j = list.size(); j < nums.length; j++) {
+                nums[j] = 0;
+            }
+        }*/
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
