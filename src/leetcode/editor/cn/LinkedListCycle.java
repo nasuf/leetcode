18ed73da0c7f117bbadb454fcf0e31f03f7b8684
pocketContributor <842100455@qq.com>
2021-09-26 10:45:43 +0800

    leetcode [141] 环形链表 - 快慢指针解法

--- src/leetcode/editor/cn/LinkedListCycle.java
+++ src/leetcode/editor/cn/LinkedListCycle.java
@@ -76,7 +76,10 @@ public class LinkedListCycle {
     }*/
 
     public class Solution {
-        public boolean hasCycle(ListNode head) {
+        /**
+         * hashset解法
+         */
+        /*public boolean hasCycle(ListNode head) {
             Set<ListNode> set = new HashSet<>();
             while (head != null) {
                 if (!set.add(head)) {
@@ -85,8 +88,30 @@ public class LinkedListCycle {
                 head = head.next;
             }
             return false;
+        }*/
+
+        /**
+         * 双指针解法
+         */
+        public boolean hasCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while (slow != fast) {
+                if (fast == null || fast.next == null) {
+                    return false;
+                }
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
         }
     }
+
+
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
