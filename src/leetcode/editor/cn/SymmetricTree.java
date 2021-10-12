3cdcf2e7160a1e9e1138940f317dd7ec9126400f
pocketContributor <842100455@qq.com>
2021-10-12 20:58:48 +0800

    leetcode [101] 对称二叉树 - 递归算法

---
+++ src/leetcode/editor/cn/SymmetricTree.java
@@ -0,0 +1,90 @@
+//给定一个二叉树，检查它是否是镜像对称的。
+//
+// 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+// 
+//
+// 
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？ 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树
+
+
+package leetcode.editor.cn;
+
+import javax.xml.soap.Node;
+import java.util.*;
+
+public class SymmetricTree {
+    public static void main(String[] args) {
+        Solution solution = new SymmetricTree().new Solution();
+        TreeNode node = new TreeNode(1);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(2);
+        node.left.left = null;
+        node.left.right = new TreeNode(3);
+        node.right.left = null;
+        node.right.right = new TreeNode(3);
+        solution.isSymmetric(node);
+//        System.out.println(null == null);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            return check(root, root);
+        }
+
+        public boolean check(TreeNode p, TreeNode q) {
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
