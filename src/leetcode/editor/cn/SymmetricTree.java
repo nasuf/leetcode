45836bf59f181aa9157c515ed9c9ae4ed7f51403
pocketContributor <842100455@qq.com>
2021-10-12 21:02:22 +0800

    leetcode [101] 对称二叉树 - 迭代算法

--- src/leetcode/editor/cn/SymmetricTree.java
+++ src/leetcode/editor/cn/SymmetricTree.java
@@ -52,7 +52,10 @@ public class SymmetricTree {
 
 
     class Solution {
-        public boolean isSymmetric(TreeNode root) {
+        /**
+         * 递归算法
+         */
+        /*public boolean isSymmetric(TreeNode root) {
             return check(root, root);
         }
 
@@ -64,6 +67,36 @@ public class SymmetricTree {
                 return false;
             }
             return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+        }*/
+
+        /**
+         * 迭代算法
+         */
+        public boolean isSymmetric(TreeNode root) {
+            return check(root, root);
+        }
+
+        public boolean check(TreeNode u, TreeNode v) {
+            Queue<TreeNode> q = new LinkedList<TreeNode>();
+            q.offer(u);
+            q.offer(v);
+            while (!q.isEmpty()) {
+                u = q.poll();
+                v = q.poll();
+                if (u == null && v == null) {
+                    continue;
+                }
+                if ((u == null || v == null) || (u.val != v.val)) {
+                    return false;
+                }
+
+                q.offer(u.left);
+                q.offer(v.right);
+
+                q.offer(u.right);
+                q.offer(v.left);
+            }
+            return true;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
