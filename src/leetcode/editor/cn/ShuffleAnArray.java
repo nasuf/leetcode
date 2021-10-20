012f19e03473dc4bd5b01c2ba4aa7edab7215ed9
pocketContributor <842100455@qq.com>
2021-10-20 19:27:50 +0800

    leetcode [384] 打乱数组 - Fisher-Yates 洗牌算法

--- src/leetcode/editor/cn/ShuffleAnArray.java
+++ src/leetcode/editor/cn/ShuffleAnArray.java
@@ -54,15 +54,16 @@ public class ShuffleAnArray {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
-        /**
-         * 暴力法
-         */
+
         private int[] array;
         private int[] original;
 
         private Random rand = new Random();
 
-        private List<Integer> getArrayCopy() {
+        /**
+         * 暴力法
+         */
+        /*private List<Integer> getArrayCopy() {
             List<Integer> asList = new ArrayList<Integer>();
             for (int i = 0; i < array.length; i++) {
                 asList.add(array[i]);
@@ -90,6 +91,37 @@ public class ShuffleAnArray {
                 aux.remove(removeIdx);
             }
 
+            return array;
+        }*/
+
+        /**
+         * Fisher-Yates 洗牌算法
+         */
+        private int randRange(int min, int max) {
+            return rand.nextInt(max - min) + min;
+        }
+
+        private void swapAt(int i, int j) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        public Solution(int[] nums) {
+            array = nums;
+            original = nums.clone();
+        }
+
+        public int[] reset() {
+            array = original;
+            original = original.clone();
+            return original;
+        }
+
+        public int[] shuffle() {
+            for (int i = 0; i < array.length; i++) {
+                swapAt(i, randRange(i, array.length));
+            }
             return array;
         }
     }
