0f74dbd20d92f726369c9b96cb1229ee6aff7770
pocketContributor <842100455@qq.com>
2021-10-27 20:51:17 +0800

    leetcode [144] 二叉树的前序遍历 - 递归算法

---
+++ src/leetcode/editor/cn/BinaryTreePreorderTraversal.java
@@ -0,0 +1,107 @@
+//给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,null,2,3]
+//输出：[1,2,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1]
+//输出：[1]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：root = [1,2]
+//输出：[1,2]
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：root = [1,null,2]
+//输出：[1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 深度优先搜索 二叉树
+
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreePreorderTraversal().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+
+        /**
+         * 递归算法
+         */
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            preorderTraversal(root, list);
+            return list;
+        }
+
+        public void preorderTraversal(TreeNode root, List<Integer> result) {
+            if (root == null) {
+                return;
+            }
+            result.add(root.val);
+            preorderTraversal(root.left, result);
+            preorderTraversal(root.right, result);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
