126dd4bca1a10e2e916cfaf21cb411f608ad034b
pocketContributor <842100455@qq.com>
2021-10-27 20:26:55 +0800

    leetcode [700] 二叉搜索树中的搜索 - 迭代算法

--- src/leetcode/editor/cn/SearchInABinarySearchTree.java
+++ src/leetcode/editor/cn/SearchInABinarySearchTree.java
@@ -54,13 +54,22 @@ public class SearchInABinarySearchTree {
     }
 
     class Solution {
-        public TreeNode searchBST(TreeNode root, int val) {
+        // 递归算法
+        /*public TreeNode searchBST(TreeNode root, int val) {
             if (root == null) return null;
             if (root.val == val) return root;
             if (val > root.val)
                 return searchBST(root.right, val);
             else
                 return searchBST(root.left, val);
+        }*/
+
+        // 迭代算法
+        public TreeNode searchBST(TreeNode root, int val) {
+            while (root != null && root.val != val) {
+                root = val > root.val ? root.right : root.left;
+            }
+            return root;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
