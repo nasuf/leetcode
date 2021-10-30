20dee3f0e87912d0c98c2eb950208d5b6b2e3360
pocketContributor <842100455@qq.com>
2021-10-30 09:15:17 +0800

    refactor code

--- src/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
+++ src/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -51,16 +51,6 @@ public class RemoveNthNodeFromEndOfList {
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     * int val;
-     * ListNode next;
-     * ListNode() {}
-     * ListNode(int val) { this.val = val; }
-     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * } 1->2->3->4  1->2->4
-     */
     class Solution {
         public ListNode removeNthFromEnd(ListNode head, int n) {
             ListNode dummy = new ListNode(0, head);
@@ -82,20 +72,4 @@ public class RemoveNthNodeFromEndOfList {
 
 //leetcode submit region end(Prohibit modification and deletion)
 
-}
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
 }
