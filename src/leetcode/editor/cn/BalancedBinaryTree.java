cd5ab7fe4c4bd9e9dd9c8574c743c20dd6702b17
pocketContributor <842100455@qq.com>
2021-10-30 08:39:26 +0800

    leetcode [110] 平衡二叉树 - 自顶向下算法

--- src/leetcode/editor/cn/BalancedBinaryTree.java
+++ src/leetcode/editor/cn/BalancedBinaryTree.java
@@ -49,8 +49,21 @@ public class BalancedBinaryTree {
     //leetcode submit region begin(Prohibit modification and deletion)
 
     class Solution {
+        /**
+         * 自顶向下
+         */
         public boolean isBalanced(TreeNode root) {
+            if (root == null)
+                return true;
+            else
+                return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
 
+        public int height(TreeNode root) {
+            if (root == null)
+                return 0;
+            else
+                return Math.max(height(root.left), height(root.right)) + 1;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
