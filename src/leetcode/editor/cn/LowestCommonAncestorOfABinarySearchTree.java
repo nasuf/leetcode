c6a2d358a12ab658c29b6fe46de09bedb0aa8985
pocketContributor <842100455@qq.com>
2021-11-15 20:59:54 +0800

    leetcode [235] 二叉搜索树的最近公共祖先 - 循环遍历算法

--- src/leetcode/editor/cn/LowestCommonAncestorOfABinarySearchTree.java
+++ src/leetcode/editor/cn/LowestCommonAncestorOfABinarySearchTree.java
@@ -43,15 +43,28 @@ public class LowestCommonAncestorOfABinarySearchTree {
     //leetcode submit region begin(Prohibit modification and deletion)
 
     class Solution {
-        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        /*public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
             if ((root.val >= p.val && root.val <= q.val) || (root.val >= q.val && root.val <= p.val)) return root;
             if (root.val > p.val && root.val > q.val)
                 return lowestCommonAncestor(root.left, p, q);
             else
                 return lowestCommonAncestor(root.right, p, q);
+        }*/
+
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            for (; ; ) {
+                if (root.val < p.val && root.val < q.val) {
+                    root = root.right;
+                } else if (root.val > p.val && root.val > q.val) {
+                    root = root.left;
+                } else {
+                    break;
+                }
+            }
+            return root;
         }
     }
-    
+
     //leetcode submit region end(Prohibit modification and deletion)
 
 }
