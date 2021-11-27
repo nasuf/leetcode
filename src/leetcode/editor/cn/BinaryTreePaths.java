53e4324b10e86b8e3c0652f57dcdf85837207cfd
pocketContributor <842100455@qq.com>
2021-11-27 11:17:40 +0800

    leetcode [257] 二叉树的所有路径 - 深度优先算法

---
+++ src/leetcode/editor/cn/BinaryTreePaths.java
@@ -0,0 +1,78 @@
+//给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+//
+// 叶子节点 是指没有子节点的节点。 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,3,null,5]
+//输出：["1->2->5","1->3"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：["1"]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数目在范围 [1, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+// Related Topics 树 深度优先搜索 字符串 二叉树 
+// 👍 602 👎 0
+
+
+package leetcode.editor.cn;
+
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePaths {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreePaths().new Solution();
+        /**
+         *     1
+         *   2   3
+         *    5
+         */
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+        solution.binaryTreePaths(root);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    class Solution {
+        public List<String> binaryTreePaths(TreeNode root) {
+            List<String> paths = new ArrayList<String>();
+            constructPaths(root, "", paths);
+            return paths;
+        }
+
+        public void constructPaths(TreeNode root, String path, List<String> paths) {
+            if (root != null) {
+                StringBuffer pathSB = new StringBuffer(path);
+                pathSB.append(Integer.toString(root.val));
+                if (root.left == null && root.right == null) {  // 当前节点是叶子节点
+                    paths.add(pathSB.toString());  // 把路径加入到答案中
+                } else {
+                    pathSB.append("->");  // 当前节点不是叶子节点，继续递归遍历
+                    constructPaths(root.left, pathSB.toString(), paths);
+                    constructPaths(root.right, pathSB.toString(), paths);
+                }
+            }
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
