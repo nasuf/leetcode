2e3650b24e087f0c5019e1489588d031161889f6
pocketContributor <842100455@qq.com>
2021-12-28 20:34:17 +0800

    leetcode [103] 二叉树的锯齿形层序遍历

---
+++ src/leetcode/editor/cn/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,69 @@
+//给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回锯齿形层序遍历如下： 
+//
+// 
+//[
+//  [3],
+//  [20,9],
+//  [15,7]
+//]
+// 
+// Related Topics 树 广度优先搜索 二叉树 
+// 👍 569 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeZigzagLevelOrderTraversal().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    class Solution {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            List<List<Integer>> ret = new ArrayList<List<Integer>>();
+            if (root == null) return ret;
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            boolean reverse = false;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> level = new ArrayList<>();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    if (reverse)
+                        level.add(0, node.val);
+                    else
+                        level.add(node.val);
+                    if (node.left != null)
+                        queue.offer(node.left);
+                    if (node.right != null)
+                        queue.offer(node.right);
+                }
+                ret.add(level);
+                reverse  = !reverse;
+            }
+            return ret;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
