044303429ece5b1e4f87786dd0edecf966ab7994
pocketContributor <842100455@qq.com>
2021-12-28 20:25:10 +0800

    leetcode [107] 二叉树的层序遍历II

---
+++ src/leetcode/editor/cn/BinaryTreeLevelOrderTraversalIi.java
@@ -0,0 +1,96 @@
+//给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其自底向上的层序遍历为： 
+//
+// 
+//[
+//  [15,7],
+//  [9,20],
+//  [3]
+//]
+// 
+// Related Topics 树 广度优先搜索 二叉树 
+// 👍 523 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.*;
+
+public class BinaryTreeLevelOrderTraversalIi {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeLevelOrderTraversalIi().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    class Solution {
+        /*public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            List<List<Integer>> ret = new ArrayList<List<Integer>>();
+            Stack<List<Integer>> stack = new Stack<>();
+            if (root == null) {
+                return ret;
+            }
+
+            Queue<TreeNode> queue = new LinkedList<TreeNode>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                List<Integer> level = new ArrayList<Integer>();
+                int currentLevelSize = queue.size();
+                for (int i = 1; i <= currentLevelSize; ++i) {
+                    TreeNode node = queue.poll();
+                    level.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                stack.push(level);
+            }
+            while (!stack.isEmpty()) {
+                ret.add(stack.pop());
+            }
+            return ret;
+        }*/
+
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            List<List<Integer>> ret = new ArrayList<List<Integer>>();
+            if (root == null) {
+                return ret;
+            }
+
+            Queue<TreeNode> queue = new LinkedList<TreeNode>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                List<Integer> level = new ArrayList<Integer>();
+                int currentLevelSize = queue.size();
+                for (int i = 1; i <= currentLevelSize; ++i) {
+                    TreeNode node = queue.poll();
+                    level.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                ret.add(0, level);
+            }
+            return ret;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
