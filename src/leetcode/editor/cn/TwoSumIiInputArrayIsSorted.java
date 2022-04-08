6521b2d48bde2ad4dc96ca9e58eb1fd9a6c7a0a8
nasuf <shanian713@gmail.com>
2022-04-08 20:42:53 +0800

    leetcode [167] 两数之和II

--- src/leetcode/editor/cn/TwoSumIiInputArrayIsSorted.java
+++ src/leetcode/editor/cn/TwoSumIiInputArrayIsSorted.java
@@ -55,7 +55,7 @@ public class TwoSumIiInputArrayIsSorted {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public int[] twoSum(int[] numbers, int target) {
+        /*public int[] twoSum(int[] numbers, int target) {
             int[] result = new int[2];
             for (int i = 0; i < numbers.length; i++) {
                 int rightIndex = binarySearch(numbers, i + 1, numbers.length - 1, numbers[i], target);
@@ -83,6 +83,23 @@ public class TwoSumIiInputArrayIsSorted {
                 }
             }
             return -1;
+        }*/
+
+        public int[] twoSum(int[] numbers, int target) {
+            for (int i = 0; i < numbers.length; ++i) {
+                int low = i + 1, high = numbers.length - 1;
+                while (low <= high) {
+                    int mid = (high - low) / 2 + low;
+                    if (numbers[mid] == target - numbers[i]) {
+                        return new int[]{i + 1, mid + 1};
+                    } else if (numbers[mid] > target - numbers[i]) {
+                        high = mid - 1;
+                    } else {
+                        low = mid + 1;
+                    }
+                }
+            }
+            return new int[]{-1, -1};
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
