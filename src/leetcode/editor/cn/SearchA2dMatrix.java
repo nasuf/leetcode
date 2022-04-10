85207e3bc65fb4990908529abc5ab6b34bbe83bf
nasuf <shanian713@gmail.com>
2022-04-10 20:30:34 +0800

    leetcode [74] 搜索二维矩阵

--- src/leetcode/editor/cn/SearchA2dMatrix.java
+++ src/leetcode/editor/cn/SearchA2dMatrix.java
@@ -36,13 +36,13 @@ public class SearchA2dMatrix {
     public static void main(String[] args) {
         Solution solution = new SearchA2dMatrix().new Solution();
         System.out.println(solution.searchMatrix(new int[][]{
-                {1,1}, {3,3}
+                {1, 1}, {3, 3}
         }, 3));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public boolean searchMatrix(int[][] matrix, int target) {
+        /*public boolean searchMatrix(int[][] matrix, int target) {
             int width = matrix[0].length;
             int height = matrix.length;
             if (target < matrix[0][0] || target > matrix[height - 1][width - 1]) {
@@ -92,6 +92,23 @@ public class SearchA2dMatrix {
                     h = mid - 1;
                 } else {
                     l = mid + 1;
+                }
+            }
+            return false;
+        }*/
+
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int m = matrix.length, n = matrix[0].length;
+            int low = 0, high = m * n - 1;
+            while (low <= high) {
+                int mid = (high - low) / 2 + low;
+                int x = matrix[mid / n][mid % n];
+                if (x < target) {
+                    low = mid + 1;
+                } else if (x > target) {
+                    high = mid - 1;
+                } else {
+                    return true;
                 }
             }
             return false;
