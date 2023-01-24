15c14b0373269536903c4cd427db9d1465583937
nasuf <shanian713@gmail.com>
2023-01-24 10:33:57 +0800

    leetcode [剑指offer 25] 合并两个排序的链表

---
+++ src/leetcode/editor/cn/HeBingLiangGePaiXuDeLianBiaoLcof.java
@@ -0,0 +1,50 @@
+/**
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ * <p>
+ * 示例1：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 链表长度 <= 1000
+ * <p>
+ * 注意：本题与主站 21 题相同：https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * <p>
+ * Related Topics 递归 链表 👍 310 👎 0
+ */
+package leetcode.editor.cn;
+
+public class HeBingLiangGePaiXuDeLianBiaoLcof {
+    public static void main(String[] args) {
+        Solution solution = new HeBingLiangGePaiXuDeLianBiaoLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            } else if (l2 == null) {
+                return l1;
+            } else if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
