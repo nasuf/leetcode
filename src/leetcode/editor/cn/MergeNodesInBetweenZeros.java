828f627b3bf374d4034bd12393b0bf44d97b70c8
nasuf <shanian713@gmail.com>
2023-03-04 15:31:17 +0800

    leetcode [2181] 合并零之间的节点

---
+++ src/leetcode/editor/cn/MergeNodesInBetweenZeros.java
@@ -0,0 +1,86 @@
+/**
+ * 给你一个链表的头节点 head ，该链表包含由 0 分隔开的一连串整数。链表的 开端 和 末尾 的节点都满足 Node.val == 0 。
+ * <p>
+ * 对于每两个相邻的 0 ，请你将它们之间的所有节点合并成一个节点，其值是所有已合并节点的值之和。然后将所有 0 移除，修改后的链表不应该含有任何 0 。
+ * <p>
+ * 返回修改后链表的头节点 head 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [0,3,1,0,4,5,2,0]
+ * 输出：[4,11]
+ * 解释：
+ * 上图表示输入的链表。修改后的链表包含：
+ * - 标记为绿色的节点之和：3 + 1 = 4
+ * - 标记为红色的节点之和：4 + 5 + 2 = 11
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [0,1,0,3,0,2,2,0]
+ * 输出：[1,3,4]
+ * 解释：
+ * 上图表示输入的链表。修改后的链表包含：
+ * - 标记为绿色的节点之和：1 = 1
+ * - 标记为红色的节点之和：3 = 3
+ * - 标记为黄色的节点之和：2 + 2 = 4
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 列表中的节点数目在范围 [3, 2 * 10⁵] 内
+ * 0 <= Node.val <= 1000
+ * 不 存在连续两个 Node.val == 0 的节点
+ * 链表的 开端 和 末尾 节点都满足 Node.val == 0
+ * <p>
+ * <p>
+ * Related Topics 链表 模拟 👍 20 👎 0
+ */
+package leetcode.editor.cn;
+
+public class MergeNodesInBetweenZeros {
+    public static void main(String[] args) {
+        Solution solution = new MergeNodesInBetweenZeros().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeNodes(ListNode head) {
+            if (head == null) return null;
+            ListNode cur = head;
+            ListNode res = new ListNode(0);
+            ListNode ans = res;
+            while (cur.next != null) {
+                if (cur.val == 0) {
+                    cur = cur.next;
+                    res.next = new ListNode(0);
+                    res = res.next;
+                    continue;
+                } else {
+                    res.val += cur.val;
+                }
+                cur = cur.next;
+            }
+            return ans.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
