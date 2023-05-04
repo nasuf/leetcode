cdced11fcfadda5b25a50893b469b76fc30cd097
nasuf <shanian713@gmail.com>
2023-05-04 21:03:59 +0800

    leetcode [1143] 最长公共子序列

---
+++ src/leetcode/editor/cn/LongestCommonSubsequence.java
@@ -0,0 +1,76 @@
+/**
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ * <p>
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * <p>
+ * <p>
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ * <p>
+ * <p>
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：text1 = "abcde", text2 = "ace"
+ * 输出：3
+ * 解释：最长公共子序列是 "ace" ，它的长度为 3 。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：text1 = "abc", text2 = "abc"
+ * 输出：3
+ * 解释：最长公共子序列是 "abc" ，它的长度为 3 。
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：text1 = "abc", text2 = "def"
+ * 输出：0
+ * 解释：两个字符串没有公共子序列，返回 0 。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 1 <= text1.length, text2.length <= 1000
+ * text1 和 text2 仅由小写英文字符组成。
+ * <p>
+ * <p>
+ * Related Topics 字符串 动态规划 👍 1308 👎 0
+ */
+package leetcode.editor.cn;
+
+public class LongestCommonSubsequence {
+    public static void main(String[] args) {
+        Solution solution = new LongestCommonSubsequence().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int longestCommonSubsequence(String text1, String text2) {
+            int m = text1.length();
+            int n = text2.length();
+            int[][] dp = new int[m + 1][n + 1];
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+            return dp[m][n];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
