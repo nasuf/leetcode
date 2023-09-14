09a56170b3148dd8448ecfa01e0b142f29050810
nasuf <shanian713@gmail.com>
2023-09-14 19:50:31 +0800

    leetcode [169] 多数元素[哈希法]

---
+++ src/leetcode/editor/cn/MajorityElement.java
@@ -0,0 +1,69 @@
+/**
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ * <p>
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：nums = [3,2,3]
+ * 输出：3
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：nums = [2,2,1,1,1,2,2]
+ * 输出：2
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * n == nums.length
+ * 1 <= n <= 5 * 10⁴
+ * -10⁹ <= nums[i] <= 10⁹
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。
+ * <p>
+ * Related Topics 数组 哈希表 分治 计数 排序 👍 1959 👎 0
+ */
+package leetcode.editor.cn;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        Solution solution = new MajorityElement().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int majorityElement(int[] nums) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (map.containsKey(nums[i])) {
+                    map.put(nums[i], map.get(nums[i]) + 1);
+                } else {
+                    map.put(nums[i], 1);
+                }
+            }
+            Map.Entry<Integer, Integer> maxEntry = null;
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (maxEntry == null || entry.getValue() > maxEntry.getValue()) {
+                    maxEntry = entry;
+                }
+            }
+            return maxEntry.getKey();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
