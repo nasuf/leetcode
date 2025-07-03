36f65386bd99fd81d43041ba60410096d9d3cd2e
nasuf <shanian713@gmail.com>
2025-07-03 09:30:08 +0800

    leetcode [48] 旋转图像

--- src/leetcode/editor/cn/RotateImage.java
+++ src/leetcode/editor/cn/RotateImage.java
@@ -66,20 +66,18 @@ public class RotateImage {
     class Solution {
         public void rotate(int[][] matrix) {
             int n = matrix.length;
-            // 水平翻转
-            for (int i = 0; i < n / 2; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    int temp = matrix[i][j];
+            for (int i = 0; i < n / 2; i++) {
+                for (int j = 0; j < n; j++) {
+                    int tmp = matrix[i][j];
                     matrix[i][j] = matrix[n - i - 1][j];
-                    matrix[n - i - 1][j] = temp;
+                    matrix[n - i - 1][j] = tmp;
                 }
             }
-            // 主对角线翻转
-            for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < i; ++j) {
-                    int temp = matrix[i][j];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    int tmp = matrix[i][j];
                     matrix[i][j] = matrix[j][i];
-                    matrix[j][i] = temp;
+                    matrix[j][i] = tmp;
                 }
             }
         }
